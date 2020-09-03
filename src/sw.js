24ccdc90e7bbc69a6500c4e804fa5cdce071a305
Suleman Rasheed <bcsf17m040@pucit.edu.pk>
2020-09-03 23:45:42 +0500

    Created the fetch caching

--- src/sw.js
+++ src/sw.js
@@ -14,3 +14,16 @@ self.addEventListener('install', (event) => {
         })
     )
 })
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.open(dynamicCache).then(function (cache) {
+            return cache.match(event.request).then(function (response) {
+                return response || fetch(event.request).then(function (response) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
