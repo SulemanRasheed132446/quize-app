49b36dcff9e4ed25d35a1c31cd5ed9636de989b1
Suleman Rasheed <bcsf17m040@pucit.edu.pk>
2020-09-03 21:23:07 +0500

    New way

--- src/sw.js
+++ src/sw.js
@@ -4,14 +4,10 @@
 const dynamicCache = 'Quizee-Dynamic-Cache';
 const assets = [
     'https://opentdb.com/api.php?amount=10&category=18&type=multiple&difficulty=easy',
-    'https://opentdb.com/api.php?amount=10&category=18&type=multiple&difficulty=medium',
-    'https://opentdb.com/api.php?amount=10&category=18&type=multiple&difficulty=hard',
-    'https://quizee.imfast.io/easy_template.pdf',
-    'https://quizee.imfast.io/medium_template.pdf',
-    'https://quizee.imfast.io/hard_template.pdf',
+    'https://opentdb.com/api_category.php'
 ];
 
-this.addEventListener('install', (event) => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(dynamicCache).then((cache) => {
             cache.addAll(assets);
